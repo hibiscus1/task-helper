e3ede6431602dee7f3ce488a688dd17480c777cc
hibiscus1 <ajackerm@gmail.com>
2025-01-26 08:01:56 -0700

    Added tasks to data

--- data.js
+++ data.js
@@ -7,8 +7,8 @@ var data = [
         "startTime": "05:00",
         "recurrence": [
             {
-                "frequency": 1,
-                "units": "week",
+                "every": 1,
+                "unit": "day",
                 "daysOfWeek": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
             }
         ],
@@ -49,11 +49,11 @@ var data = [
         "title": "Evening Routine",
         "description": "Wind Down",
         "flexibility": "strict",
-        "startTime": "20:00",
+        "startTime": "21:00",
         "recurrence": [
             {
-                "frequency": 1,
-                "units": "week",
+                "every": 1,
+                "unit": "day",
                 "daysOfWeek": ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"]
             }
         ],
@@ -70,20 +70,92 @@ var data = [
             },
             {
                 "id": 3,
-                "title": "Stretch"
+                "title": "Wash Face"
             }
         ]
     },
     {
         "id": 3,
         "title": "Carpal Exercises",
-        "flexibility": "strict",
-        "startTime": "20:00",
+        "category": "health",
+        "flexibility": "flexible",
+        "priority": 10,
         "recurrence": [
             {
-                "frequency": 1,
-                "units": "week",
+                "every": 2,
+                "unit": "day",
                 "daysOfWeek": ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"]
+            }
+        ]
+        
+    },
+    {
+        "id": 4,
+        "title": "Meditation",
+        "flexibility": "flexible",
+        "priority": 10,
+        "recurrence": [
+            {
+                "every": 1,
+                "unit": "day",
+                "daysOfWeek": ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"]
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "title": "Voice Training",
+        "duration": 30,
+        "flexibility": "flexible",
+        "priority": 16,
+        "recurrence": [
+            {
+                "every": 1,
+                "unit": "day",
+                "daysOfWeek": ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"]
+            }
+        ]
+    },
+    {
+        "id": 6,
+        "title": "Sketchbook",
+        "category": "hobbies",
+        "duration": 60,
+        "flexibility": "flexible",
+        "priority": 20,
+        "recurrence": [
+            {
+                "every": 1,
+                "unit": "day",
+                "daysOfWeek": ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"]
+            }
+        ]
+    },
+    {
+        "id": 7,
+        "title": "Reading",
+        "category": "hobbies",
+        "duration": 60,
+        "flexibility": "flexible",
+        "priority": 20,
+        "recurrence": [
+            {
+                "every": 1,
+                "unit": "day",
+                "daysOfWeek": ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"]
+            }
+        ]
+    },
+    {
+        "id": 8,
+        "title": "Sheets Laundry",
+        "flexibility": "flexible",
+        "priority": 4,
+        "recurrence": [
+            {
+                "every": 2,
+                "unit": "week",
+                "daysOfWeek": ["Sun", "Sat"]
             }
         ],
         "subtasks":
@@ -91,18 +163,134 @@ var data = [
             
             {
                 "id": 1,
-                "title": "Floss"
+                "title": "Load Washer",
+                "after": "hold"
             },
             {
                 "id": 2,
-                "title": "Brush Teeth"
+                "title": "Load Dryer",
+                "after": "hold"
             },
             {
                 "id": 3,
-                "title": "Stretch"
+                "title": "Make the Bed"
             }
         ]
-    }
+    },
+    {
+        "id": 9,
+        "title": "Clothes Laundry",
+        "flexibility": "flexible",
+        "priority": 6,
+        "recurrence": [
+            {
+                "every": 1,
+                "unit": "week",
+                "daysOfWeek": ["Sun", "Sat"]
+            }
+        ],
+        "subtasks":
+        [
+            
+            {
+                "id": 1,
+                "title": "Load Washer",
+                "after": "hold"
+            },
+            {
+                "id": 2,
+                "title": "Load Dryer",
+                "after": "hold"
+            },
+            {
+                "id": 3,
+                "title": "Fold and Put Away"
+            }
+        ]
+    },
+    {
+        "id": 10,
+        "title": "Towels Laundry",
+        "flexibility": "flexible",
+        "priority": 6,
+        "recurrence": [
+            {
+                "every": 1,
+                "unit": "week",
+                "daysOfWeek": ["Sun", "Sat"]
+            }
+        ],
+        "subtasks":
+        [
+            
+            {
+                "id": 1,
+                "title": "Load Washer",
+                "after": "hold"
+            },
+            {
+                "id": 2,
+                "title": "Load Dryer",
+                "after": "hold"
+            },
+            {
+                "id": 3,
+                "title": "Fold and Put Away"
+            }
+        ]
+    },
+    {
+        "id": 11,
+        "title": "Go For a Walk",
+        "flexibility": "flexible",
+        "priority": 16,
+        "recurrence": [
+            {
+                "every": 1,
+                "unit": "day",
+                "daysOfWeek": ["Sun", "Sat"]
+            }
+        ]
+    },
+    {
+        "id": 12,
+        "title": "Clean Floors",
+        "flexibility": "flexible",
+        "priority": 12,
+        "recurrence": [
+            {
+                "every": 2,
+                "unit": "weeks",
+                "daysOfWeek": ["Sun", "Sat"]
+            }
+        ],
+        "subtasks":
+        [
+            
+            {
+                "id": 1,
+                "title": "Vacuum"
+            },
+            {
+                "id": 2,
+                "title": "Mop"
+            }
+        ]
+    },
+    {
+        "id": 13,
+        "title": "Clean Out Fridge",
+        "category": "cleaning",
+        "flexibility": "flexible",
+        "priority": 14,
+        "recurrence": [
+            {
+                "every": 2,
+                "unit": "weeks",
+                "daysOfWeek": ["Sun", "Sat"]
+            }
+        ]
+    },
 
 ]
 
